6ac60c7d508a8ac779c247886b56d0023bfe2d90
hemanth_kumar <hemanth4125@gmail.com>
2024-09-28 20:59:26 +0530

    infra development

---
+++ 20-sg/security.tf
@@ -0,0 +1,9 @@
+module "mysql_test" {
+  source = "git::https://github.com/HemanthKumar-75/terraform-aws-security-group.git?ref=main"
+  envinronment = var.envinronment
+  project_name = var.project
+  security_group = "Mysql"
+  vpc_id = local.vpc_id
+  common_tags = var.common_tags
+  sg_tags = var.sg_tags
+}
